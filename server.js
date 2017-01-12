0f7d140fc99bf29244118b8c9fddf0701cca66ba
Alex Kim <alexkimwrites@gmail.com>
2017-01-12 12:43:19 -0800

    Added functional signup/unsub on frontend.

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
+const app            = require('express')()
+const bodyParser     = require('body-parser')
+const methodOverride = require('method-override')
+const pg             = require('pg')
+const knex           = require('knex')({
+  client: 'postgresql',
+  connection: {
+    user: 'Alex1',
+    database: 'fitnessbot'
+  }
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
 
-app.post('/db', function (req, res) {
-  console.log(req.body)
-  res.json(req.body)
+app.post('/signup', function (req, res) {
+  knex('users')
+    .insert({name: req.body.name, age: req.body.age, mobile: req.body.num})
+    .returning('name')
+    .then(() => res.sendStatus(201))
 })
 
-app.listen(3000, () => {console.log('Listening on port 3000')})
+app.delete('/unsub', function (req, res) {
+  knex('users')
+    .where('mobile', '=', req.body.num)
+    .del()
+    .then(() => res.sendStatus(204))
+})
+
+
+app.listen(3000, () => { console.log('Listening on port 3000')})
