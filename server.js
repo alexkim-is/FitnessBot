d438632378a6c0e25c90c6e202c9737a5885db05
Alex Kim <alexkimwrites@gmail.com>
2017-01-13 17:00:32 -0800

    Added 3 images and text

--- server.js
+++ server.js
@@ -14,19 +14,23 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 
-app.post('/signup', function (req, res) {
+//SERVER respondds to FRONTEND when a request is made at URL.
+app.post('/signup', function (req, res)  {
   knex('users')
-    .insert({name: req.body.name, age: req.body.age, mobile: req.body.num})
+    .insert({
+      name: req.body.name,
+      age: req.body.age,
+      mobile: req.body.num.replace(/-/g, '')
+    })
     .returning('name')
-    .then(() => res.sendStatus(201))
+    .then(() => res.send('Thank you for joining'))
 })
 
 app.delete('/unsub', function (req, res) {
   knex('users')
     .where('mobile', '=', req.body.num)
     .del()
-    .then(() => res.sendStatus(204))
+    .then(() => res.send('See you again.'))
 })
-
 
 app.listen(3000, () => { console.log('Listening on port 3000')})
