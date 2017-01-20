df6819c11590c8d318914eb334d4a1980c6de510
Alex Kim <alexkimwrites@gmail.com>
2017-01-20 13:11:51 -0800

    last commit no changes

--- public/index.js
+++ public/index.js
@@ -2,9 +2,11 @@
 //Message at the top
 var daysPassed = new Date().getDate()
 var hoursLeft = 23.5 - new Date().getHours()
+var message = document.querySelector('#message')
 var clockMessage = document.getElementById('clock-message')
 clockMessage.textContent = daysPassed + " days have passed in 2017. You have "
 + hoursLeft + " hours to get in a great workout today!"
+message.appendChild(clockMessage)
 
 //Selecting Fitness Goals
 var $goals = document.getElementById('goals')
